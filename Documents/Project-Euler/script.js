b198e659dc403d3c42f9865af7118d9b2b84fed9
Lisa Ramos <lsrms3977@gmail.com>
2017-06-20 14:44:06 -0400

    Added README

---
+++ Documents/Project-Euler/script.js
@@ -0,0 +1,88 @@
+// Hiding and showing first answer
+function answerOne() {
+    $('.hidden1').hide();
+    $('.hidden1').fadeIn(1000);
+
+    $('.hidden1').show();
+
+  $(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).next.slideToggle(500);
+        $(this).toggleClass('hidden1');
+    })
+  });
+}
+
+$(document).ready(main);
+
+// Hiding and showing second answer
+
+function answerTwo() {
+    $('.hidden2').hide();
+    $('.hidden2').fadeIn(1000);
+
+    $('.hidden2').show();
+
+  $(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).next.slideToggle(500);
+        $(this).toggleClass('hidden2');
+    })
+  });
+}
+
+$(document).ready(main);
+
+// Hiding and showing third answer
+
+function answerThree() {
+    $('.hidden3').hide();
+    $('.hidden3').fadeIn(1000);
+
+    $('.hidden3').show();
+
+  $(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).next.slideToggle(500);
+        $(this).toggleClass('hidden3');
+    })
+  });
+}
+
+$(document).ready(main);
+
+// Hiding and showing fourth answer
+
+function answerFour() {
+    $('.hidden4').hide();
+    $('.hidden4').fadeIn(1000);
+
+    $('.hidden4').show();
+
+  $(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).next.slideToggle(500);
+        $(this).toggleClass('hidden4');
+    })
+  });
+}
+
+$(document).ready(main);
+
+// Hiding and showing fifth answer
+
+function answerFive() {
+    $('.hidden5').hide();
+    $('.hidden5').fadeIn(1000);
+
+    $('.hidden5').show();
+
+  $(document).ready(function() {
+    $('button').on('click', function() {
+        $(this).next.slideToggle(500);
+        $(this).toggleClass('hidden5');
+    })
+  });
+}
+
+$(document).ready(main);
